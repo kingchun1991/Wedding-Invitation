482a5dc13021580ee6977b2195a4196f27d20902
King Chun O <kingchun1991@hotmail.com>
2023-06-06 22:04:50 +0800

    counter

---
+++ src/lib/layout/CountdownTimer.tsx
@@ -0,0 +1,79 @@
+import {
+    Box,
+    StatGroup,
+    StatNumber,
+    Stat,
+    StatHelpText,
+  } from '@chakra-ui/react';
+  import type React from 'react';
+  import { useState, useEffect } from 'react';
+  
+  interface TimeRemaining {
+    total: number;
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+  
+  interface CountdownTimerProps {
+    targetDate: Date;
+  }
+  
+  const CountdownTimer: React.FC<CountdownTimerProps> = ({ targetDate }) => {
+    const getTimeRemaining = (endDate: Date): TimeRemaining => {
+      const total =
+        Date.parse(endDate.toString()) - Date.parse(new Date().toString());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(total / (1000 * 60 * 60 * 24));
+      return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds,
+      };
+    };
+  
+    const [timeRemaining, setTimeRemaining] = useState<TimeRemaining>(
+      getTimeRemaining(targetDate)
+    );
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setTimeRemaining(getTimeRemaining(targetDate));
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, [targetDate]);
+  
+    return (
+      <Box marginRight="auto">
+        <Box>
+          <StatGroup gap={8}>
+            <Stat>
+              <StatNumber>{timeRemaining.days}</StatNumber>
+              <StatHelpText>days</StatHelpText>
+            </Stat>
+            <Stat>
+              <StatNumber>{timeRemaining.hours}</StatNumber>{' '}
+              <StatHelpText>hours </StatHelpText>
+            </Stat>
+            <Stat>
+              <StatNumber>{timeRemaining.minutes}</StatNumber>{' '}
+              <StatHelpText>minutes</StatHelpText>
+            </Stat>
+            <Stat>
+              <StatNumber>{timeRemaining.seconds}</StatNumber>
+              <StatHelpText> seconds</StatHelpText>
+            </Stat>
+          </StatGroup>
+        </Box>
+      </Box>
+    );
+  };
+  
+  export default CountdownTimer;
+  
--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,10 +1,22 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 
+import CountdownTimer from './Countdown';
 import ThemeToggle from './ThemeToggle';
 
 const Header = () => {
+  const targetDate = new Date();
+  // set the target date to 11 Nov 2023
+  targetDate.setDate(11);
+  targetDate.setMonth(11);
+  targetDate.setFullYear(2023);
+  targetDate.setHours(17);
+  targetDate.setMinutes(30);
   return (
     <Flex as="header" width="full" align="center">
+      <CountdownTimer targetDate={targetDate} />
+      <Heading as="h1" size="md">
+        Waiwai & Frank
+      </Heading>
       <Box marginLeft="auto">
         <ThemeToggle />
       </Box>
