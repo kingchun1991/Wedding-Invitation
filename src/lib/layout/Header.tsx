eb2059b6e9f2e02a6a5495c31f4c5c01a358c3c4
King Chun O <kingchun1991@hotmail.com>
2023-06-06 22:20:19 +0800

    remove

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 
 import CountdownTimer from './CountdownTimer';
 import ThemeToggle from './ThemeToggle';
