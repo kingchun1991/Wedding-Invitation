b200cd53c3c6d715926f919d2592fc01acebd696
Frank O <kingchun1991@hotmail.com>
2023-09-18 21:57:13 +0800

    feat: svg

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,25 +1,25 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "Wedding-Invitation",
-  titleTemplate: "%s | Wedding-Invitation",
-  defaultTitle: "Wedding-Invitation",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://wedding-invitation-orpin.vercel.app/",
+  title: 'Wedding-Invitation',
+  titleTemplate: '%s | Wedding-Invitation',
+  defaultTitle: 'Wedding-Invitation',
+  description: 'Next.js + chakra-ui + TypeScript template',
+  canonical: 'https://wedding-invitation-orpin.vercel.app/',
   openGraph: {
-    url: "https://wedding-invitation-orpin.vercel.app/",
-    title: "Wedding-Invitation",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: 'https://wedding-invitation-orpin.vercel.app/',
+    title: 'Wedding-Invitation',
+    description: 'Next.js + chakra-ui + TypeScript template',
     images: [
       {
-        url: "https://wedding-invitation-orpin.vercel.app/main.png",
-        alt: "Wedding-Invitation og-image",
+        url: 'https://wedding-invitation-orpin.vercel.app/main.svg',
+        alt: 'Wedding-Invitation og-image',
       },
     ],
-    site_name: "Wedding-Invitation",
+    site_name: 'Wedding-Invitation',
   },
   twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
+    handle: '@sozonome',
+    cardType: 'summary_large_image',
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,9 +36,8 @@ export const metadata: Metadata = {
     title: 'Waiwai & Frank Wedding Invitation',
     description: `${description}`,
     images: {
-      url: 'https://wedding-invitation-orpin.vercel.app/main.png',
+      url: 'https://wedding-invitation-orpin.vercel.app/main.svg',
       alt: 'Waiwai & Frank Wedding og-image',
-      type: 'png',
     },
   },
   twitter: {
@@ -46,8 +45,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     description: `${description}`,
     images: {
-      url: 'https://wedding-invitation-orpin.vercel.app/main.png',
-      type: ' png',
+      url: 'https://wedding-invitation-orpin.vercel.app/main.svg',
     },
   },
 };
