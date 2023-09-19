60789e3d8d2b142a9e3f9325490737ffb41aa0da
okc280 <120083074+okc280@users.noreply.github.com>
2023-09-19 14:31:28 +0800

    feat: png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     title: 'Waiwai & Frank Wedding Invitation',
     description: `${description}`,
     images: {
-      url: 'https://wedding-invitation-orpin.vercel.app/api/og',
+      url: 'https://wedding-invitation-orpin.vercel.app/api/png',
       alt: 'Waiwai & Frank Wedding og-image',
       type: 'png',
       width: '1200',
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     description: `${description}`,
     images: {
-      url: 'https://wedding-invitation-orpin.vercel.app/api/og',
+      url: 'https://wedding-invitation-orpin.vercel.app/api/png',
       alt: 'Waiwai & Frank Wedding og-image',
       type: 'png',
       width: '1200',
