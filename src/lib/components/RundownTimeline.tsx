483a8bea26dc830392717aa7b7611fa9b091d011
Frank O <kingchun1991@hotmail.com>
2023-10-02 08:22:49 +0800

    feat: update RundownTimeline.tsx

--- src/lib/components/RundownTimeline.tsx
+++ src/lib/components/RundownTimeline.tsx
@@ -41,7 +41,7 @@ const RundownTimeline = ({ events }: { events: IEvent[] }) => {
   });
 
   return (
-    <Stepper index={activeStep} orientation="vertical" height="400px" gap="0">
+    <Stepper index={activeStep} orientation="vertical" gap="0">
       {displayEvents.map((event) => (
         <Step key={event.ID}>
           <StepIndicator>
@@ -63,34 +63,24 @@ const RundownTimeline = ({ events }: { events: IEvent[] }) => {
                 <h2>
                   <AccordionButton>
                     <Box as="span" flex="1" textAlign="left">
-                      Section 1 title
+                      Morning
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
+                <AccordionPanel pb={4}></AccordionPanel>
               </AccordionItem>
 
               <AccordionItem>
                 <h2>
                   <AccordionButton>
                     <Box as="span" flex="1" textAlign="left">
-                      Section 2 title
+                      Afternoon
                     </Box>
                     <AccordionIcon />
                   </AccordionButton>
                 </h2>
-                <AccordionPanel pb={4}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </AccordionPanel>
+                <AccordionPanel pb={4}></AccordionPanel>
               </AccordionItem>
             </Accordion> */}
           </Box>
