957566a27eef10a090a1ff8a40ef3c6b2a4749f6
Frank O <kingchun1991@hotmail.com>
2023-10-02 08:33:12 +0800

    feat: update RundownTimeline.tsx

--- src/lib/components/RundownTimeline.tsx
+++ src/lib/components/RundownTimeline.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable radix */
 /* eslint-disable react/no-array-index-key */
 import {
-  // Accordion,
-  // AccordionButton,
-  // AccordionIcon,
-  // AccordionItem,
-  // AccordionPanel,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Step,
   StepDescription,
@@ -34,6 +35,20 @@ const RundownTimeline = ({ events }: { events: IEvent[] }) => {
   const displayEvents: IEvent[] = events.filter(
     (event) => event.Description !== undefined && event.Description !== ''
   );
+  const morningEvents: IEvent[] = displayEvents.filter((event) => {
+    const fromTime = parseInt(event.From.split(':')[0]);
+    return fromTime >= 4 && fromTime < 12;
+  });
+
+  const afternoonEvents: IEvent[] = displayEvents.filter((event) => {
+    const fromTime = parseInt(event.From.split(':')[0]);
+    return fromTime >= 12 && fromTime < 18;
+  });
+
+  const eveningEvents: IEvent[] = displayEvents.filter((event) => {
+    const fromTime = parseInt(event.From.split(':')[0]);
+    return fromTime >= 18 && fromTime <= 23;
+  });
 
   const { activeStep } = useSteps({
     index: 1,
@@ -41,54 +56,115 @@ const RundownTimeline = ({ events }: { events: IEvent[] }) => {
   });
 
   return (
-    <Stepper index={activeStep} orientation="vertical" gap="0">
-      {displayEvents.map((event) => (
-        <Step key={event.ID}>
-          <StepIndicator>
-            <StepStatus
-              complete={<MdCheck />}
-              incomplete={event.From}
-              active={event.From}
-            />
-          </StepIndicator>
+    <Accordion allowToggle>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left">
+              Morning
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <Stepper index={activeStep} orientation="vertical" gap="0">
+            {morningEvents.map((event) => (
+              <Step key={event.ID}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<MdCheck />}
+                    incomplete={event.From}
+                    active={event.From}
+                  />
+                </StepIndicator>
 
-          <Box flexShrink="0">
-            <StepTitle
-              style={{ whiteSpace: 'pre-line' }}
-              dangerouslySetInnerHTML={{ __html: event.Description }}
-            />
-            <StepDescription>{event.Location}</StepDescription>
-            {/* <Accordion allowToggle>
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Morning
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}></AccordionPanel>
-              </AccordionItem>
+                <Box flexShrink="0">
+                  <StepTitle
+                    style={{ whiteSpace: 'pre-line' }}
+                    dangerouslySetInnerHTML={{ __html: event.Description }}
+                  />
+                  <StepDescription>{event.Location}</StepDescription>
+                </Box>
 
-              <AccordionItem>
-                <h2>
-                  <AccordionButton>
-                    <Box as="span" flex="1" textAlign="left">
-                      Afternoon
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}></AccordionPanel>
-              </AccordionItem>
-            </Accordion> */}
-          </Box>
+                <StepSeparator />
+              </Step>
+            ))}
+          </Stepper>
+        </AccordionPanel>
+      </AccordionItem>
+      {/* //afternoonEvents */}
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left">
+              Afternoon
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <Stepper index={activeStep} orientation="vertical" gap="0">
+            {afternoonEvents.map((event) => (
+              <Step key={event.ID}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<MdCheck />}
+                    incomplete={event.From}
+                    active={event.From}
+                  />
+                </StepIndicator>
 
-          <StepSeparator />
-        </Step>
-      ))}
-    </Stepper>
+                <Box flexShrink="0">
+                  <StepTitle
+                    style={{ whiteSpace: 'pre-line' }}
+                    dangerouslySetInnerHTML={{ __html: event.Description }}
+                  />
+                  <StepDescription>{event.Location}</StepDescription>
+                </Box>
+
+                <StepSeparator />
+              </Step>
+            ))}
+          </Stepper>
+        </AccordionPanel>
+      </AccordionItem>
+      {/* //eveningEvents */}
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as="span" flex="1" textAlign="left">
+              Evening
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <Stepper index={activeStep} orientation="vertical" gap="0">
+            {eveningEvents.map((event) => (
+              <Step key={event.ID}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<MdCheck />}
+                    incomplete={event.From}
+                    active={event.From}
+                  />
+                </StepIndicator>
+
+                <Box flexShrink="0">
+                  <StepTitle
+                    style={{ whiteSpace: 'pre-line' }}
+                    dangerouslySetInnerHTML={{ __html: event.Description }}
+                  />
+                  <StepDescription>{event.Location}</StepDescription>
+                </Box>
+
+                <StepSeparator />
+              </Step>
+            ))}
+          </Stepper>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
